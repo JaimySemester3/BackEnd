13b44c3c116f14be5e7050bcfbecc48d1dbb1f32
JaimyDerks <78917274+JaimyDerks@users.noreply.github.com>
2023-09-26 23:42:45 +0200

    Added database CRUD

---
+++ agenda-backend/PlannerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace agenda_backend
+{
+    public class PlannerDbContext : DbContext
+    {
+        public PlannerDbContext(DbContextOptions<PlannerDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>()
+                .ToTable("task");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
---
+++ agenda-backend/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace agenda_backend
+{
+    public class Task
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
